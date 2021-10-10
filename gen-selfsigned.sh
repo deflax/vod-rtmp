e518c6defd85989564912a96cd93c3375e089569
deflax <daniel@deflax.net>
2021-10-10 15:51:28 +0000

    cleanup selfsigned on creation

--- gen-selfsigned.sh
+++ gen-selfsigned.sh
@@ -6,3 +6,8 @@ openssl genrsa -out default.key 2048
 openssl req -new -key default.key -out default.csr
 openssl x509 -req -days 3650 -in default.csr -signkey default.key -out default.crt
 cat default.key default.crt >> default.pem
+
+#cleanup
+rm default.key
+rm default.csr
+rm default.crt
